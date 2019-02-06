2c9368ad949df697f7774a91d6ddb19ece7756d2
Travis CI User <travis@example.org>
2019-02-06 14:24:40 +0000

    Update documentation

---
+++ source-files.js
@@ -0,0 +1,8 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex['aligned'] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex['bare_metal'] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex['cortex_m'] = {"name":"","dirs":[{"name":"peripheral","dirs":[],"files":["cbp.rs","cpuid.rs","dcb.rs","dwt.rs","fpb.rs","fpu.rs","itm.rs","mod.rs","mpu.rs","nvic.rs","scb.rs","syst.rs","tpiu.rs"]},{"name":"register","dirs":[],"files":["apsr.rs","basepri.rs","basepri_max.rs","control.rs","faultmask.rs","lr.rs","mod.rs","msp.rs","pc.rs","primask.rs","psp.rs"]}],"files":["asm.rs","interrupt.rs","itm.rs","lib.rs","macros.rs"]};
+sourcesIndex['cortex_m_semihosting'] = {"name":"","dirs":[],"files":["debug.rs","export.rs","hio.rs","lib.rs","macros.rs","nr.rs"]};
+sourcesIndex['panic_semihosting'] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex['vcell'] = {"name":"","dirs":[],"files":["lib.rs"]};
+sourcesIndex['volatile_register'] = {"name":"","dirs":[],"files":["lib.rs"]};
